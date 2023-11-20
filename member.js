8fc5e2760fe6c33098199274932f07dcc3109dc2
kz3eXodd <kz9exodd@gmail.com>
2023-11-20 07:44:51 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberButton = document.getElementById("memberButton");
+    var skillsButton = document.getElementById("skillsButton");
+    var projectsButton = document.getElementById("projectsButton");
+    var contactButton = document.getElementById("contactButton");
+    var aboutButton = document.getElementById("aboutButton");
+    var memberButton = document.getElementById("memberButton");
+    var skillsButton = document.getElementById("skillsButton");
+    var projectsButton = document.getElementById("projectsButton");
+    var contactButton = document.getElementById("contactButton");
+    var aboutButton = document.getElementById("aboutButton");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    memberButton.style.backgroundColor = "#1a1a1a";
+    skillsButton.style.backgroundColor = "#333333";
+    projectsButton.style.backgroundColor = "#333333";
+    contactButton.style.backgroundColor = "#333333";
+    aboutButton.style.backgroundColor = "#333333";
+}
